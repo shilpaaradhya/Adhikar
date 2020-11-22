daee71817997fd3caa4de4742b6bbcb3996e6aa2
shilpaaradhya <shilpaaradhya411@gmail.com>
2020-11-22 22:08:07 +0530

    Api Fetching

--- Movies/src/app/app.component.ts
+++ Movies/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component ,  OnInit  } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Movies';
+  apiUrl ="https://k.adhikar.net/movies/";
+  movies : any = []
+
+  constructor(  private http : HttpClient ) {}
+
+  ngOnInit(): void {
+    this. onSearch()
+  }
+
+  onSearch(){
+    this.http.get(this.apiUrl).subscribe((data : any) => {
+      console.log(data);
+      this.movies = data;
+      console.log(this.movies)
+    })
+  }
 }
+
+
